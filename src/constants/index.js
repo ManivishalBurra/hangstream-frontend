352dc2496af020281021886291a28f2fcce36462
manivishal <manivishalbits@gmail.com>
2021-10-21 13:54:52 +0530

    constants removed

--- src/constants/index.js
+++
@@ -1,3 +0,0 @@
-//export const BASE_URL = "http://localhost:7000";
-// export const BASE_URL = 'https://sheltered-cliffs-75906.herokuapp.com';
-export const BASE_URL = "https://hangstream-backend.herokuapp.com";
