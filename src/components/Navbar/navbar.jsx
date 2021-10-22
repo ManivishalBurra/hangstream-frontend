2feb38ed5c156c572ab26e4b3b15c4dc096c2a98
manivishal <manivishalbits@gmail.com>
2021-10-22 12:26:36 +0530

    update 3.0.1

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -51,122 +51,135 @@ const Navbar = () => {
   }
   function DynamicSearch(e) {
     SetSearchText(e.target.value);
+    var u = users.filter((list) => {
+      var x = list.username.replace(/\s+/g, "").trim().toLowerCase();
+      return x.includes(
+        e.target.value.replace(/\s+/g, "").trim().toLowerCase()
+      );
+    });
+    setSearchResult(u);
   }
 
   return (
-    <div>
-      <div
-        className="navigation-wrap start-header start-style center"
-        id={theme}
-      >
-        <div class="nav-container">
-          <div class="row">
-            <div class="col-12">
-              <nav class="navbar navbar-expand-md navbar-light">
-                <Link class="navbar-brand center" to={"/home/" + userid}>
-                  <img src={logo} alt="" />
-                  &nbsp;Hangstream
-                </Link>
+    <>
+      <div>
+        <div
+          className="navigation-wrap start-header start-style center"
+          id={theme}
+        >
+          <div class="nav-container">
+            <div class="row">
+              <div class="col-12">
+                <nav class="navbar navbar-expand-md navbar-light">
+                  <Link class="navbar-brand center" to={"/home/" + userid}>
+                    <img src={logo} alt="" />
+                    &nbsp;Hangstream
+                  </Link>
 
-                <button
-                  class="navbar-toggler"
-                  type="button"
-                  data-toggle="collapse"
-                  data-target="#navbarSupportedContent"
-                  aria-controls="navbarSupportedContent"
-                  aria-expanded="false"
-                  aria-label="Toggle navigation"
-                >
-                  <span class="navbar-toggler-icon"></span>
-                </button>
+                  <button
+                    class="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                  >
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
 
-                <div
-                  class="collapse navbar-collapse"
-                  id="navbarSupportedContent"
-                >
-                  <ul class="navbar-nav ml-auto py-4 py-md-0">
-                    <li class="nav-item pl-4 pl-md-0 ml-0 ml-md-4">
-                      <Link class="nav-link " to={"/home/" + userid}>
-                        Home
-                      </Link>
-                    </li>
-
-                    <li class="nav-item pl-4 pl-md-0 ml-0 ml-md-4">
-                      <Link class="nav-link " to={"/movies"}>
-                        Streamflix
-                      </Link>
-                    </li>
-
-                    <li class="nav-item pl-4 pl-md-0 ml-0 ml-md-4 ">
-                      <a
-                        class="nav-link dropdown-toggle"
-                        data-toggle="dropdown"
-                        href="#"
-                        role="button"
-                        aria-haspopup="true"
-                        aria-expanded="false"
-                      >
-                        Account
-                        <AccountCircleOutlinedIcon />
-                      </a>
-                      <div class="dropdown-menu" id={theme + "-dropdown"}>
-                        <a class="dropdown-item" onClick={Logout}>
-                          Logout
-                        </a>
-                        <Link class="dropdown-item" to={"/account"}>
-                          My account
+                  <div
+                    class="collapse navbar-collapse"
+                    id="navbarSupportedContent"
+                  >
+                    <ul class="navbar-nav ml-auto py-4 py-md-0">
+                      <li class="nav-item pl-4 pl-md-0 ml-0 ml-md-4">
+                        <Link class="nav-link " to={"/home/" + userid}>
+                          Home
                         </Link>
-                      </div>
-                    </li>
-                    <li class="nav-item pl-4 pl-md-0 ml-0 ml-md-4">
-                      <Link class="nav-link " onClick={Themer}>
-                        {darkStatus ? (
-                          <Brightness2Icon
-                            style={{
-                              transition: "2s",
-                              color: "#d9dbdc",
-                              transform: "rotate(45deg)",
-                            }}
-                          />
-                        ) : (
-                          <WbSunnyIcon className="theme-choice" />
-                        )}
-                      </Link>
-                    </li>
-                    <li class=" pl-4 pl-md-0 ml-0 ml-md-4">
-                      <Link
-                        class="nav-link "
-                        onClick={() => setSearchbar("searchbar-expand")}
-                      >
-                        {darkStatus ? (
-                          <SearchIcon
-                            style={{
-                              transition: "2s",
-                              color: "#d9dbdc",
-                            }}
-                          />
-                        ) : (
-                          <SearchIcon className="theme-choice" />
-                        )}
-                      </Link>
-                      <input
-                        className={searchbar}
-                        name="episode"
-                        placeholder="Search user or movie"
-                        value={searchText}
-                        onChange={DynamicSearch}
-                        onBlur={() => setSearchbar("searchbar-close")}
-                      />
-                    </li>
-                  </ul>
-                </div>
-              </nav>
+                      </li>
+
+                      <li class="nav-item pl-4 pl-md-0 ml-0 ml-md-4">
+                        <Link class="nav-link " to={"/movies"}>
+                          Streamflix
+                        </Link>
+                      </li>
+
+                      <li class="nav-item pl-4 pl-md-0 ml-0 ml-md-4 ">
+                        <a
+                          class="nav-link dropdown-toggle"
+                          data-toggle="dropdown"
+                          href="#"
+                          role="button"
+                          aria-haspopup="true"
+                          aria-expanded="false"
+                        >
+                          Account
+                          <AccountCircleOutlinedIcon />
+                        </a>
+                        <div class="dropdown-menu" id={theme + "-dropdown"}>
+                          <a class="dropdown-item" onClick={Logout}>
+                            Logout
+                          </a>
+                          <Link class="dropdown-item" to={"/account"}>
+                            My account
+                          </Link>
+                        </div>
+                      </li>
+                      <li class="nav-item pl-4 pl-md-0 ml-0 ml-md-4">
+                        <Link class="nav-link " onClick={Themer}>
+                          {darkStatus ? (
+                            <Brightness2Icon
+                              style={{
+                                transition: "2s",
+                                color: "#d9dbdc",
+                                transform: "rotate(45deg)",
+                              }}
+                            />
+                          ) : (
+                            <WbSunnyIcon className="theme-choice" />
+                          )}
+                        </Link>
+                      </li>
+                      <li class=" pl-4 pl-md-0 ml-0 ml-md-4">
+                        <Link
+                          class="nav-link "
+                          onClick={() => setSearchbar("searchbar-expand")}
+                        >
+                          {darkStatus ? (
+                            <SearchIcon
+                              style={{
+                                transition: "2s",
+                                color: "#d9dbdc",
+                              }}
+                            />
+                          ) : (
+                            <SearchIcon className="theme-choice" />
+                          )}
+                        </Link>
+                        <input
+                          className={searchbar + " " + theme + "-search"}
+                          name="episode"
+                          placeholder="Search user or movie"
+                          value={searchText}
+                          onChange={DynamicSearch}
+                          onBlur={() => setSearchbar("searchbar-close")}
+                          autoComplete="off"
+                        />
+                      </li>
+                    </ul>
+                  </div>
+                </nav>
+              </div>
             </div>
           </div>
         </div>
+        <div className="gap-filler" />
       </div>
-      <div className="gap-filler" />
-    </div>
+      {searchResult.length > 0 && searchText.length > 0 && (
+        <SearchResult result={searchResult} />
+      )}
+    </>
   );
 };
 
