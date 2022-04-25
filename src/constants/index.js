1f9ebefaf26117deac68703f955e8a074984ef6f
Burra Vishal <burra.vishal@MB-M-BURRAVISHAL.mshome.net>
2022-04-25 20:51:49 +0530

    token added

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,7 +2,7 @@
 export const AGORA = {
   NAME: "main",
   API_ID: "578ab0ce63e14e62877e8f529b7e446a",
-  TEMP: "006578ab0ce63e14e62877e8f529b7e446aIAABFxfmVDslchlYUgOYtHvLkoJYvFDefo8otNSiIAz6M2TNKL8AAAAAEADchOPNWZFmYgEAAQBYkWZi",
+  TEMP: "006578ab0ce63e14e62877e8f529b7e446aIADv1TStDjzxwf8craa+6R8CAze6L74VM+Pkw3kM/8G6nGTNKL8AAAAAEADchOPNiQ1oYgEAAQCJDWhi",
 };
 // export const BASE_URL = 'https://sheltered-cliffs-75906.herokuapp.com';
 export const BASE_URL = "https://hangstream-backend.herokuapp.com";
