d32dee0c5491f04f9ca92f962fe663f9d9a64ef8
ManivishalBurra <72870001+ManivishalBurra@users.noreply.github.com>
2022-09-23 13:43:08 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,5 +4,5 @@ export const AGORA = {
   API_ID: "578ab0ce63e14e62877e8f529b7e446a",
   TEMP: "006578ab0ce63e14e62877e8f529b7e446aIADv1TStDjzxwf8craa+6R8CAze6L74VM+Pkw3kM/8G6nGTNKL8AAAAAEADchOPNiQ1oYgEAAQCJDWhi",
 };
-export const BASE_URL = 'http://localhost:7993';
-// export const BASE_URL = "https://hangstream-backend.vercel.app";
+// export const BASE_URL = 'http://localhost:7993';
+export const BASE_URL = "https://hangstream-backend.vercel.app";
